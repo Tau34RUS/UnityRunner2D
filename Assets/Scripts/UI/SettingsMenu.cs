d8351ad82353a7356eb42a180e622c135b7e15e2
Vlad <tau34rus@gmail.com>
2018-05-23 18:42:19 +0300

    Audio changes

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -7,9 +7,21 @@ public class SettingsMenu : MonoBehaviour {
 
 	public AudioMixer audioMixer;
 
+	public float stored_volume;
+
+	void Start () {
+
+		if(PlayerPrefs.HasKey("MainVolume")){
+
+			stored_volume = PlayerPrefs.GetFloat ("MainVolume");
+			audioMixer.SetFloat ("SFXVolume", stored_volume);
+		}
+	}
+
 	public void SetVolume (float volume) {
 
 		audioMixer.SetFloat ("SFXVolume", volume);
+		PlayerPrefs.SetFloat ("MainVolume", volume);
 
 	}
 
